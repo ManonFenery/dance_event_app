de8d34f5ec83660509994825eb5c670a3e0259aa
manonfenery <fenery.manon@gmail.com>
2019-03-08 11:11:15 +0100

    strongs parameters for admin and user sign_up and update_account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+	before_action :devise_parameter_sanitizer, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+
+	protected
+
+		def configure_permitted_parameters
+			if current_user
+    		devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :style, :level])
+    		devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :birthdate, :style, :level])
+    	else
+    		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    		devise_parameter_sanitizer.permit(:saccount_update, keys: [:name, :description, :contact_mail, :social, :website])
+    	end
+  	end
 end
